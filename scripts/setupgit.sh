f716a4f802bdfa808db096d9f6abe4eb67b0bdde
hunghm <hungmanh.aitech@gmail.com>
2025-07-27 11:13:28 +0000

    add default branch

--- scripts/setupgit.sh
+++ scripts/setupgit.sh
@@ -1,5 +1,6 @@
 git remote add git@github.com:AE-AI-HIT15/hit-product-group-2.git
 
+git branch -m main
 git config --global user.email "hungmanh.aitech@gmail.com"
 git config --global user.name "hunghm"
 
